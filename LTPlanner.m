6c5364d57d2804e908c334c8f1c1f60245d40f90
Yannick Burkhardt <yannick.burkhardt@tum.de>
2022-04-19 21:12:12 +0100

    bug fixes

--- LTPlanner.m
+++ LTPlanner.m
@@ -265,7 +265,7 @@ classdef LTPlanner < handle
             t = zeros(obj.DoF,7); % Absolute time that is required to reach the end of current jerk phase
             mod_jerk_profile = false;
             eps = 1e-4;
-            tol = 0.001;
+            tol = 0.05;
 
             %% Analyse input data
             % Check if inputs are in limits
@@ -291,7 +291,7 @@ classdef LTPlanner < handle
 
                 %% Calculate required v_drive to reach goal at given time
                 % Standard jerk profile: Phases 2 and 6 exist
-                v_drive = (96*a_0(i)*obj.a_max(i) + 24*a_0(i)*obj.j_max(i) + 96*obj.j_max(i)*v_0(i) - 3^(1/2)*(48*a_0(i)*obj.j_max(i)^3 - 3072*a_0(i)*obj.a_max(i)^3 + 1536*a_0(i)^3*obj.j_max(i) + 32*obj.a_max(i)*obj.j_max(i)^3 + 1536*obj.a_max(i)^3*obj.j_max(i) - 192*obj.j_max(i)^3*v_0(i) + 1536*a_0(i)^4 - 1280*obj.a_max(i)^4 + 3*obj.j_max(i)^4 + 3072*a_0(i)^2*obj.a_max(i)^2 + 384*a_0(i)^2*obj.j_max(i)^2 - 288*obj.a_max(i)^2*obj.j_max(i)^2 - 3072*obj.j_max(i)^2*v_0(i)^2 - 1536*a_0(i)*obj.j_max(i)^2*v_0(i) + 3072*obj.a_max(i)*obj.j_max(i)^2*v_0(i) - 9216*obj.a_max(i)^2*obj.j_max(i)*v_0(i) + 3072*obj.a_max(i)^2*obj.j_max(i)^2*t_required^2 + 576*a_0(i)*obj.a_max(i)*obj.j_max(i)^2 - 2304*a_0(i)*obj.a_max(i)^2*obj.j_max(i) + 1536*a_0(i)^2*obj.a_max(i)*obj.j_max(i) + 192*obj.a_max(i)*obj.j_max(i)^3*t_required - 9216*obj.a_max(i)^3*obj.j_max(i)*t_required + 6144*a_0(i)*obj.a_max(i)*obj.j_max(i)*v_0(i) + 1536*a_0(i)*obj.a_max(i)*obj.j_max(i)^2*t_required + 6144*a_0(i)*obj.a_max(i)^2*obj.j_max(i)*t_required + 12288*obj.a_max(i)*dir(i)*obj.j_max(i)^2*q_0(i) - 12288*obj.a_max(i)*dir(i)*obj.j_max(i)^2*q_goal + 6144*obj.a_max(i)*obj.j_max(i)^2*t_required*v_0(i))^(1/2) - 144*obj.a_max(i)^2 + 3*obj.j_max(i)^2 + 96*obj.a_max(i)*obj.j_max(i)*t_required)/(192*obj.j_max(i));
+                v_drive = (obj.a_max(i)*obj.j_max(i)*t_required/2 - a_0(i)^2/4 + a_0(i)*obj.a_max(i)/2 - obj.a_max(i)^2/2 + v_0(i)*obj.j_max(i)/2 - sqrt(36*obj.a_max(i)^2*obj.j_max(i)^2*t_required^2 - 36*a_0(i)^2*obj.a_max(i)*obj.j_max(i)*t_required + 72*a_0(i)*obj.a_max(i)^2*obj.j_max(i)*t_required - 72*obj.a_max(i)^3*obj.j_max(i)*t_required + 144*obj.a_max(i)*dir(i)*obj.j_max(i)^2*q_0(i) - 144*obj.a_max(i)*dir(i)*obj.j_max(i)^2*q_goal(i) + 72*obj.a_max(i)*obj.j_max(i)^2*v_0(i)*t_required - 9*a_0(i)^4 + 12*a_0(i)^3*obj.a_max(i) + 36*a_0(i)^2*obj.a_max(i)^2 + 36*a_0(i)^2*obj.j_max(i)*v_0(i) - 72*a_0(i)*obj.a_max(i)^3 - 72*a_0(i)*obj.a_max(i)*obj.j_max(i)*v_0(i) + 36*obj.a_max(i)^4 - 36*obj.j_max(i)^2*v_0(i)^2)/12)/obj.j_max(i);
                 
                 if ~imag(v_drive)
                     t = optSwitchTimes(obj, q_goal, q_0, dir*v_0, dir*a_0, v_drive);
@@ -384,6 +384,9 @@ classdef LTPlanner < handle
                         continue;
                     end
                 end
+                
+                % No valid solution found
+                t = zeros(obj.DoF,7);
             end
         end
         
