4cfc2a2b4ca3cdd3b030fdf19211e736c848a3f4
JakobThumm <jakob.thumm@web.de>
2022-07-20 16:34:14 +0200

    Added 4th root function and tests

--- include/long_term_planner/roots.h
+++ include/long_term_planner/roots.h
@@ -6,6 +6,7 @@
 #include <boost/math/tools/roots.hpp>
 #include <boost/math/special_functions/next.hpp>
 #include <boost/math/special_functions/cbrt.hpp>
+#include <boost/math/special_functions/pow.hpp>
 
 #ifndef roots_H
 #define roots_H
@@ -40,32 +41,32 @@ struct fourth_functor_2deriv {
            a_3 * boost::math::pow<3>(x) + 
            a_2 * boost::math::pow<2>(x) + 
            a_1 * x + a_0;
-    T dx = 4 * a_4 * boost::math::pow<3>(x) +
-           3 * a_3 * boost::math::pow<2>(x) +
-           2 * a_2 * x + a_1;
-    T d2x = 12 * a_4 * boost::math::pow<2>(x) +
-            6 * a_3 * x +
-            2 * a_2;
+    T dx = 4.0 * a_4 * boost::math::pow<3>(x) +
+           3.0 * a_3 * boost::math::pow<2>(x) +
+           2.0 * a_2 * x + a_1;
+    T d2x = 12.0 * a_4 * boost::math::pow<2>(x) +
+            6.0 * a_3 * x +
+            2.0 * a_2;
     return std::make_tuple(fx, dx, d2x);
   }
 };
 
 template <class T>
-T fourth_2deriv(T x) {
+T fourth_2deriv(T a_4, T a_3, T a_2, T a_1, T a_0) {
   // return fourth root of x using 1st and 2nd derivatives and Halley.
   using namespace std;                  // Help ADL of std functions.
   using namespace boost::math::tools;   // for halley_iterate.
 
   int exponent;
-  frexp(x, &exponent);                  // Get exponent of z (ignore mantissa).
-  T guess = ldexp(1., exponent / 4);    // Rough guess is to divide the exponent by five.
-  T min = ldexp(0.5, exponent / 4);     // Minimum possible value is half our guess.
-  T max = ldexp(2., exponent / 4);      // Maximum possible value is twice our guess.
+  frexp(a_0, &exponent);                 // Get exponent of z (ignore mantissa).
+  T guess = ldexp(1., exponent / 4);     // Rough guess is to divide the exponent by four.
+  T min = 0.0;                           // Minimum possible value is zero.
+  T max = ldexp(10., exponent / 4);      // Maximum possible value is ten times our guess.
   // Stop when slightly more than one of the digits are correct:
-  const int digits = static_cast<int>(std::numeric_limits<T>::digits * 0.4);
+  const int digits = 8;
   const boost::uintmax_t maxit = 50;
   boost::uintmax_t it = maxit;
-  T result = halley_iterate(fourth_functor_2deriv<T>(x), guess, min, max, digits, it);
+  T result = halley_iterate(fourth_functor_2deriv<T>(a_4, a_3, a_2, a_1, a_0), guess, min, max, digits, it);
   return result;
 }
 
--- tests/src/roots_tests.cc
+++ tests/src/roots_tests.cc
@@ -4,8 +4,24 @@
 
 namespace long_term_planner {
 TEST(RootsTests, SimpleTest) {
-  EXPECT_EQ(0, 0);
-} 
+  float a_4 = 1.0;
+  float a_3 = 0.0;
+  float a_2 = 0.0;
+  float a_1 = 0.0;
+  float a_0 = -625.0;
+  float r = fourth_2deriv(a_4, a_3, a_2, a_1, a_0);
+  EXPECT_NEAR(r, 5, 1e-6);
+}
+
+TEST(RootsTests, ComplicatedTest) {
+  float a_4 = 5.0;
+  float a_3 = -3.0;
+  float a_2 = 1000.0;
+  float a_1 = 2.5;
+  float a_0 = -80.0;
+  float r = fourth_2deriv(a_4, a_3, a_2, a_1, a_0);
+  EXPECT_NEAR(r, 0.281658, 1e-5);
+}
 } // namespace long_term_planner
 
 int main(int argc, char **argv){
