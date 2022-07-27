a71bb243bccc4ace2b6742122f753508933ae64e
JakobThumm <jakob.thumm@web.de>
2022-07-27 13:25:26 +0200

    Finalized unit tests

--- tests/src/long_term_planner_tests.cc
+++ tests/src/long_term_planner_tests.cc
@@ -224,7 +224,6 @@ TEST_F(LongTermPlannerTest1DoF, gridTestOneJoint) {
         // Check if goal was reached
         EXPECT_NEAR(traj.q[0][traj.length-1], q_goal, tol);
         if (abs(traj.q[0][traj.length-1] - q_goal) > tol) {
-          std::cout << 3.1 << std::endl;
           std::cerr << "q_goal = " << q_goal << 
                        ", v_0 = " << v_0 << 
                        ", a_0 = " << a_0 <<
@@ -233,6 +232,90 @@ TEST_F(LongTermPlannerTest1DoF, gridTestOneJoint) {
         }
       }
     }
+  }
+}
+
+TEST_F(LongTermPlannerTest1DoF, GridTimeScalingTest) {
+  // Initialize parameters
+  double eps = 1e-6;
+  double tol_q = 0.02;
+  double tol_t = 0.1;
+  double step = 0.1;
+  std::vector<double> q_min = {-6};
+  std::vector<double> q_max = {7};
+  std::vector<double> v_max = {1.0};
+  std::vector<double> a_max = {2.0};
+  std::vector<double> j_max = {15.0};
+  double t_sample = 0.004;
+  double q_0 = 0.5;
+  std::vector<double> time_increments = {0.05, 0.1, 0.2, 0.5, 1.0, 2.0};
+
+  // Initialize planner
+  ltp_.setSampleTime(t_sample);
+  ltp_.setLimits(q_min, q_max, v_max, a_max, j_max);
+
+  // Set goal and joint angle
+  for (int i=(int)q_min[0]/step; i<=(int)q_max[0]/step; i++) {
+    double q_goal = i * step;
+    // Velocity in Limits
+    for (int j = (int)-v_max[0]/step; j<(int)v_max[0]/step; j++) {
+      double v_0 = j * step;
+      v_0 = (j>0) ? v_0 - eps : v_0 + eps; 
+      double a_lb, a_ub;
+      if (v_0 >= 0) {
+        a_lb = -(a_max[0]-eps);
+        a_ub = std::min(a_max[0]-eps, sqrt(2*j_max[0]*(v_max[0] - v_0)));
+      } else {
+        a_lb = std::max(-(a_max[0]-eps), -sqrt(2*j_max[0]*(v_max[0] - abs(v_0))));
+        a_ub = a_max[0];
+      }
+      // Acceleration in limits
+      double a_range = a_ub-a_lb;
+      int n_steps = (int) floor(a_range/step);
+      for (int k=0; k<n_steps; k++) {
+        double a_0 = a_lb + k*step;
+        std::array<double, 7> t_ltp;
+        double dir;
+        char mod_jerk_profile;
+        bool successOptSwitchTimes = ltp_.optSwitchTimes(0, q_goal, q_0, v_0, a_0, v_max[0], t_ltp, dir, mod_jerk_profile);
+        EXPECT_TRUE(successOptSwitchTimes);
+        if (!successOptSwitchTimes) continue;      
+        // Scale up trajectory
+        for (int l=0; l<time_increments.size(); l++) {
+          // Skip if already at goal position
+          if (t_ltp[t_ltp.size()-1] < tol_q) {
+            break;
+          }
+          
+          // Calculate scaled times
+          std::array<double, 7> t_scaled;
+          double v_drive;
+          char mod_jerk_profile;
+          if (i == 1 && j == -9 && k == 2 && l == 2) {
+            mod_jerk_profile = false;
+          }
+          bool successTimeScaling = ltp_.timeScaling(0, q_goal, q_0, v_0, a_0, dir, t_ltp[t_ltp.size()-1] + time_increments[l], t_scaled, v_drive, mod_jerk_profile);
+          if (!successTimeScaling) {
+            t_scaled = t_ltp;
+          }
+          // Calculate trajectories
+          std::vector<std::array<double, 7>> ts = {t_scaled};
+          std::vector<double> dirs = {dir};
+          std::vector<double> v_drives = {v_drive};
+          std::vector<char> mod_jerk_profiles = {mod_jerk_profile};
+          std::vector<double> q0s = {q_0};
+          std::vector<double> v0s = {v_0};
+          std::vector<double> a0s = {a_0};
+          Trajectory traj = ltp_.getTrajectory(ts, dirs, mod_jerk_profiles, q0s, v0s, a0s, v_drives);
+          EXPECT_NEAR(traj.q[0][traj.length-1], q_goal, tol_q);
+          if (abs(t_ltp[6] + time_increments[l] - t_scaled[6]) > tol_t) {
+            EXPECT_NEAR(traj.v[0][traj.length-1], 0.0, tol_q);
+            EXPECT_NEAR(traj.a[0][traj.length-1], 0.0, tol_q);
+            EXPECT_NEAR(t_scaled[2], t_scaled[6], eps);
+          }
+        }
+      }
+    }  
   }
 }
 
