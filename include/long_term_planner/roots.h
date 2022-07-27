8ca7e94019dab6386e41e10d3c8d79e3e7358602
JakobThumm <jakob.thumm@web.de>
2022-07-27 18:40:53 +0200

    Removed all boost.

--- include/long_term_planner/roots.h
+++ include/long_term_planner/roots.h
@@ -4,11 +4,6 @@
 #include <tuple>
 #include <complex>
 
-#include <boost/math/tools/roots.hpp>
-#include <boost/math/special_functions/next.hpp>
-#include <boost/math/special_functions/cbrt.hpp>
-#include <boost/math/special_functions/pow.hpp>
-
 #include <Eigen/Dense>
 #include <Eigen/Eigenvalues> 
 
@@ -16,124 +11,6 @@
 #define roots_H
 
 namespace long_term_planner {
-
-/**
- * @brief Functor for a 4th degree polynomial.
- * a_4 * x^4 + a_3 * x^3 + a_2 * x^2 + a_1 * x + a_0 = 0
- */
-template <class T>
-struct fourth_functor_2deriv {
- private:
-  T a_4;
-  T a_3;
-  T a_2;
-  T a_1;
-  T a_0;
- public:
-  // Functor returning both 1st and 2nd derivatives.
-  fourth_functor_2deriv(T const& a_4, T const& a_3, T const& a_2, T const& a_1, T const& a_0) : 
-    a_4(a_4),
-    a_3(a_3),
-    a_2(a_2),
-    a_1(a_1),
-    a_0(a_0) {}
-
-  std::tuple<T, T, T> operator()(T const& x) {
-    // Return f(x), f'(x) and f''(x).
-    T fx = a_4 * boost::math::pow<4>(x) + 
-           a_3 * boost::math::pow<3>(x) + 
-           a_2 * boost::math::pow<2>(x) + 
-           a_1 * x + a_0;
-    T dx = 4.0 * a_4 * boost::math::pow<3>(x) +
-           3.0 * a_3 * boost::math::pow<2>(x) +
-           2.0 * a_2 * x + a_1;
-    T d2x = 12.0 * a_4 * boost::math::pow<2>(x) +
-            6.0 * a_3 * x +
-            2.0 * a_2;
-    return std::make_tuple(fx, dx, d2x);
-  }
-};
-
-template <class T>
-T fourth_2deriv(T a_4, T a_3, T a_2, T a_1, T a_0) {
-  // return fourth root of x using 1st and 2nd derivatives and Halley.
-  using namespace std;                  // Help ADL of std functions.
-  using namespace boost::math::tools;   // for halley_iterate.
-
-  int exponent;
-  frexp(a_0, &exponent);                 // Get exponent of z (ignore mantissa).
-  T guess = ldexp(1., exponent / 4);     // Rough guess is to divide the exponent by four.
-  T min = 0.0;                           // Minimum possible value is zero.
-  T max = ldexp(10., exponent / 4);      // Maximum possible value is ten times our guess.
-  // Stop when slightly more than one of the digits are correct:
-  const int digits = 8;
-  const boost::uintmax_t maxit = 50;
-  boost::uintmax_t it = maxit;
-  T result = halley_iterate(fourth_functor_2deriv<T>(a_4, a_3, a_2, a_1, a_0), guess, min, max, digits, it);
-  return result;
-};
-
-/**
- * @brief Functor for a 5th degree polynomial.
- * a_5 * x^5 + a_4 * x^4 + a_3 * x^3 + a_2 * x^2 + a_1 * x + a_0 = 0
- */
-template <class T>
-struct fifth_functor_2deriv {
- private:
-  T a_5;
-  T a_4;
-  T a_3;
-  T a_2;
-  T a_1;
-  T a_0;
- public:
-  // Functor returning both 1st and 2nd derivatives.
-  fifth_functor_2deriv(T const& a_5, T const& a_4, T const& a_3, T const& a_2, T const& a_1, T const& a_0) : 
-    a_5(a_5),
-    a_4(a_4),
-    a_3(a_3),
-    a_2(a_2),
-    a_1(a_1),
-    a_0(a_0) {}
-
-  std::tuple<T, T, T> operator()(T const& x) {
-    // Return f(x), f'(x) and f''(x).
-    T fx = a_5 * boost::math::pow<5>(x) + 
-           a_4 * boost::math::pow<4>(x) + 
-           a_3 * boost::math::pow<3>(x) + 
-           a_2 * boost::math::pow<2>(x) + 
-           a_1 * x +
-           a_0;
-    T dx = 5.0 * a_5 * boost::math::pow<4>(x) +
-           4.0 * a_4 * boost::math::pow<3>(x) +
-           3.0 * a_3 * boost::math::pow<2>(x) +
-           2.0 * a_2 * x + a_1;
-    T d2x = 20.0 * a_5 * boost::math::pow<3>(x) +
-            12.0 * a_4 * boost::math::pow<2>(x) +
-            6.0 * a_3 * x +
-            2.0 * a_2;
-    return std::make_tuple(fx, dx, d2x);
-  }
-};
-
-template <class T>
-T fifth_2deriv(T a_5, T a_4, T a_3, T a_2, T a_1, T a_0) {
-  // return fifth root of x using 1st and 2nd derivatives and Halley.
-  using namespace std;                  // Help ADL of std functions.
-  using namespace boost::math::tools;   // for halley_iterate.
-
-  int exponent;
-  frexp(a_0, &exponent);                 // Get exponent of z (ignore mantissa).
-  T guess = ldexp(1., exponent / 5);     // Rough guess is to divide the exponent by four.
-  T min = 0.0;                           // Minimum possible value is zero.
-  T max = ldexp(10., exponent / 5);      // Maximum possible value is ten times our guess.
-  // Stop when slightly more than one of the digits are correct:
-  const int digits = 8;
-  const boost::uintmax_t maxit = 50;
-  boost::uintmax_t it = maxit;
-  T result = halley_iterate(fifth_functor_2deriv<T>(a_5, a_4, a_3, a_2, a_1, a_0), guess, min, max, digits, it);
-  return result;
-};
 
 /**
  * @brief Calculate all roots of a polynomial using the companion matrix.
--- tests/src/roots_tests.cc
+++ tests/src/roots_tests.cc
@@ -6,37 +6,6 @@
 
 namespace long_term_planner {
 
-TEST(RootsTests, SimpleTest4) {
-  float a_4 = 1.0;
-  float a_3 = 0.0;
-  float a_2 = 0.0;
-  float a_1 = 0.0;
-  float a_0 = -625.0;
-  float r = fourth_2deriv(a_4, a_3, a_2, a_1, a_0);
-  EXPECT_NEAR(r, 5, 1e-6);
-};
-
-TEST(RootsTests, ComplicatedTest4) {
-  float a_4 = 5.0;
-  float a_3 = -3.0;
-  float a_2 = 1000.0;
-  float a_1 = 2.5;
-  float a_0 = -80.0;
-  float r = fourth_2deriv(a_4, a_3, a_2, a_1, a_0);
-  EXPECT_NEAR(r, 0.281658, 1e-5);
-};
-
-TEST(RootsTests, SimpleTest5) {
-  float a_5 = 1.0;
-  float a_4 = 0.0;
-  float a_3 = 0.0;
-  float a_2 = 0.0;
-  float a_1 = 0.0;
-  float a_0 = -3125.0;
-  float r = fifth_2deriv(a_5, a_4, a_3, a_2, a_1, a_0);
-  EXPECT_NEAR(r, 5, 1e-6);
-};
-
 TEST(RootsTests, ComplexRootsTest6) {
   float a_6 = 144;
   float a_5 = -1008;
